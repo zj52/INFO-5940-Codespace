f584df56187ae0a45ed7d5d044e4558dee7662ee
zj52 <zj52@cornell.edu>
2025-09-24 16:50:39 -0400

    commit

--- streamlit_sample.py
+++ streamlit_sample.py
@@ -1,30 +1,35 @@
-
 import streamlit as st
 from openai import OpenAI
 
+
 client = OpenAI()
+
 
 st.set_page_config(page_title="Hello Codespaces", layout="centered", page_icon="👋")
 
+
 st.title("Let's chat :blush:")
 
+
 if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "Howdy partner!"}]
+   st.session_state.messages ["messages"] = [{"role": "assistant", "content": "Howdy partner!"}]
 
-for message in st.session_state["messages"]:
-    if message["role"] != "system":
-        st.chat_message(message["role"]).write(message["content"])
 
+for message in st.session_state.messages:
+   if msg["role"] != "system":
+       st.chat_message(msg['role']).write(msg['content'])
+      
 if prompt := st.chat_input():
-    st.session_state["messages"].append({"role": "user", "content": prompt})
-    st.chat_message("user").write(prompt)
+  
+   st.session_state.messages.append({"role": "user", "content": prompt})
+   st.chat_message("user").write(prompt)
 
-    with st.chat_message("assistant"):
-        stream = client.chat.completions.create(
-            model="openai-gpt-4o",
-            messages=st.session_state["messages"],
-            stream=True
-        )
-        response = st.write_stream(stream)
 
-    st.session_state["messages"].append({"role": "assistant", "content": response})
+   with st.chat_message("assistant"):
+       stream = client.chat.completions.create(model="openai-gpt-4o",
+                                               messages=st.session_state.messages,
+                                               stream=True)
+       response = st.write_stream(stream)
+
+
+   st.session_state.messages.append({"role": "assistant", "content": response})
